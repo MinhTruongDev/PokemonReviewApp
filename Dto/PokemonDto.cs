b3792811d9b8b7595903f75177fbdef9ffd90b4e
Phan Minh Trường <104303855+MinhTruongDev@users.noreply.github.com>
2023-09-24 21:24:41 +0700

    Pokemon controller done

---
+++ Dto/PokemonDto.cs
@@ -0,0 +1,12 @@
+﻿namespace PokemonReviewApp.Dto
+{
+	public class PokemonDto
+	{
+		/// <summary>Pokemon id</summary>
+		public int Id { get; set; }
+		/// <summary>Pokemon name</summary>
+		public string Name { get; set; }
+		/// <summary>Pokemon birthdate</summary>
+		public DateTime BirthDate { get; set; }
+	}
+}
---
+++ Interfaces/IPokemonRepository.cs
@@ -0,0 +1,40 @@
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Interfaces
+{
+	public interface IPokemonRepository
+	{
+		/// <summary>
+		/// Get pokemons
+		/// </summary>
+		/// <returns>List of pokemon</returns>
+		ICollection<Pokemon> GetPokemons();
+
+		/// <summary>
+		/// Get pokemon
+		/// </summary>
+		/// <param name="pokemonId">Pokemon id</param>
+		/// <returns>Pokemon</returns>
+		Pokemon GetPokemon(int pokemonId);
+		/// <summary>
+		/// Get pokemon
+		/// </summary>
+		/// <param name="pokemonName">Pokemon name</param>
+		/// <returns>Pokemon</returns>
+		Pokemon GetPokemon(string pokemonName);
+
+		/// <summary>
+		/// Get pokemon rating
+		/// </summary>
+		/// <param name="pokemonId">Pokemon id</param>
+		/// <returns>Pokemon rating</returns>
+		decimal GetPokemonRating(int pokemonId);
+
+		/// <summary>
+		/// Is this pokemon exists?
+		/// </summary>
+		/// <param name="pokemonId">Pokemon id</param>
+		/// <returns>If exist this Pokemon: TRUE, esle: FALSE</returns>
+		bool PokemonExists(int pokemonId);
+	}
+}
